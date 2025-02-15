a78398bf9d63062b3b50d6539cb48c736c071243
Kyazs <santosjohncasper@gmail.com>
2025-02-16 02:14:24 +0800

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,40 +23,10 @@ class DatabaseSeeder extends Seeder
             'role' => 'admin',
             'is_admin' => true
         ]);
-
-        // Seed client user and profile
-        $client = User::factory()->create([
-            'name' => 'John Doe',
-            'email' => 'john@gmail.com',
-            'password' => bcrypt('password'),
-            'role' => 'client'
-        ]);
-
-        ClientProfile::create([
-            'id' => $client->id,
-            'rating' => 4.5,
-            'is_suspended' => false,
-        ]);
-
-        // Seed first artist user and profile
-        $artist = User::factory()->create([
-            'name' => 'Artist User',
-            'email' => 'artist@gmail.com',
-            'password' => bcrypt('password'),
-            'role' => 'artist'
-        ]);
-
-        ArtistProfile::create([
-            'id' => $artist->id,
-            'phone_number' => '123-456-7890',
-            'location' => 'New York',
-            'gender' => 'Male',
-            'username' => 'artist1',
-            'birthdate' => '1990-01-01',
-            'bio' => 'An amazing artist.',
-            'is_suspended' => false,
-            'rating' => 5.0,
-            'available' => true,
+        
+        $this->call([
+            ArtistSeeder::class,
+            ClientSeeder::class,
         ]);
     }
 }
